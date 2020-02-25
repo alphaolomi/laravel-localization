217e74669adf9c674970cf7eae504bb23c219cd3
Alpha Olomi <alphaolomi@gmail.com>
2020-02-25 19:05:14 +0300

    feat: added static files and resouces

---
+++ resources/views/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.base')
+
+@section('content')
+{{-- <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('welcome.welcome') }}
+
+
+                    <a href="{{ LaravelLocalization::localizeUrl('(/test)') }}">@lang('Follow this link')</a>
+                    {{ LaravelLocalization::getLocalizedURL('en') }}
+                    {{ LaravelLocalization::getNonLocalizedURL('/sw/about') }}
+                    {{ LaravelLocalization::getURLFromRouteNameTranslated('es', 'routes.about') }}
+                    {{ LaravelLocalization::getSupportedLocales() }}
+                    {{ LaravelLocalization::getCurrentLocale() }}
+                    {{ LaravelLocalization::getCurrentLocaleName() }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div> --}}
+@endsection
