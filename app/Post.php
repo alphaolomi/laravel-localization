236a8cc2020b2752d30140c309e9fb1e785e3f8a
Alpha Olomi <alphaolomi@gmail.com>
2020-02-26 13:18:03 +0300

    wip: create post -a

---
+++ app/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+
+    use Sluggable;
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+}
