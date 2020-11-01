ac2a3def5ff6f01eef41ee1cec879106ec530938
Alpha Olomi <alphaolomi@gmail.com>
2020-11-01 07:59:08 +0300

    :sparkles: :zap::fire: #23

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 
 Route::group(['prefix' => LaravelLocalization::setLocale()], function () {
+    // Authentification Route
+    Auth::routes();
+
+    // Pages routes
     Route::get('/', function () {
         return view('index');
     });
@@ -18,19 +22,15 @@ Route::group(['prefix' => LaravelLocalization::setLocale()], function () {
     Route::get('/courses', function () {
         return view('courses.index');
     });
-    Auth::routes();
+
+    // Admin routes
     Route::group(['prefix' => 'admin'], function () {
         Route::get('/home', 'HomeController@index')->name('home');
         Route::resource('posts', 'Admin\PostController', ["as" => 'admin']);
         Route::resource('courses', 'Admin\CourseController', ["as" => 'admin']);
     });
-    if (App::environment('testing') || App::environment('local')) {
-        Route::get('/_testing/login', function () {
-            Auth::login(factory(\App\User::class)->create());
-        });
-        Route::get('/_testing/create', function () {
-            $modelClass = 'App\\' . request('model');
-            return factory($modelClass)->create();
-        });
-    }
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
